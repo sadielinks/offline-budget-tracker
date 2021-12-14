3576c2aeab141a02ef31ac27cdb376b02e2ff0a8
Sadie Sial <sadiesial@gmail.com>
2021-12-13 19:06:02 -0600

    activate event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,3 +29,22 @@ self.addEventListener('install', (event) => {
     );
 });
 
+self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
