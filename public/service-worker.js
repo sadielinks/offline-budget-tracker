3d64e309914b3c23c8841ce9b4a8b04c008f6cd5
Sadie Sial <sadiesial@gmail.com>
2021-12-13 19:02:45 -0600

    files to cache added from index.html + app folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,15 @@
 // service-worker.js to utilize caches to run offline
 const FILES_TO_CACHE = [
+    // files from app
     '/',
-    '/',
-    '/',
-    '/',
-    '/',
-    '/',
-    '/',
-    '/',
-    '/',
-
-]
+    '/index.html',
+    '/index.js',
+    '/indexedDb.js',
+    '/service-worker.js',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    // links within index.html doc
+    '/https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    '/https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+];
