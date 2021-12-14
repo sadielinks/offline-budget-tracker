69b7d9fbafa3c9f728fb1080cf5fc4f54a5a3260
Sadie Sial <sadiesial@gmail.com>
2021-12-13 19:04:14 -0600

    cache variables + install event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,3 +13,19 @@ const FILES_TO_CACHE = [
     '/https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     '/https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
+
+// mini project:
+const PRECACHE = 'precache-v1';
+const CACHE_NAME = "static-cache-v1";
+const RUNTIME = 'runtime';
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
