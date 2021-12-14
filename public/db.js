1af3876a770762d7e8e5b408b791072818fcea50
Sadie Sial <sadiesial@gmail.com>
2021-12-13 20:36:26 -0600

    formatted w/ prettier

--- public/db.js
+++ public/db.js
@@ -8,13 +8,13 @@ request.onupgradeneeded = (event) => {
     // create new object 'pending'
     event.target.result.createObjectStore('pending', {
         keyPath: 'id',
-        autoIncrement: true
+        autoIncrement: true,
     });
 };
 
 // IDBRequest .onerror https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest/onerror
 request.onerror = (error) => {
-    console.log(error.message)
+    console.log(error.message);
 };
 
 // IDBRequest .onsuccess https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest/onsuccess
@@ -22,38 +22,38 @@ request.onsuccess = (event) => {
     db = event.target.result;
 
     if (navigator.onLine) {
-
         catchUpDatabase();
-    };
-
+    }
+    0;
 };
 
 // offline/no network saving from public/index.js
 function saveRecord(record) {
-    // transaction gets stored to 'pending' object 
+    // transaction gets stored to 'pending' object
     const transaction = db.transaction('pending');
     const store = transaction.objectStore('pending');
     store.add(record);
-};
+}
 
 // once reconnected, add records saved inside 'pending'
 function catchUpDatabase() {
     const transaction = db.transaction('pending', 'readonly');
     const store = transaction.objectStore('pending');
     // eventually used for fetching data from offline time
-    const allData = store.allData();
+    const getAll = store.getAll();
 
     // fetch from own app api
-    allData.onsuccess = () => {
+    getAll.onsuccess = () => {
         // ignore 'empty' records that might have been saved
-        if (allData.result.length > 0) {
+        if (getAll.result.length > 0) {
             fetch('/api/transaction/bulk', {
                 method: 'POST',
-                body: JSON.stringify(allData.result),
+                body: JSON.stringify(getAll.result),
                 // added from help with TA https://developer.mozilla.org/en-US/docs/Web/API/Headers
                 headers: {
-                    Accept: "application/json, text/plain, */*", "Content-Type": "application/json"
-                }
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
             })
                 // if conditions met, results in:
                 .then((response) => response.json())
@@ -61,10 +61,10 @@ function catchUpDatabase() {
                     const transaction = db.transaction('pending', 'readwrite');
                     const store = transaction.objectStore('pending');
                     store.clear();
-                })
+                });
         }
-    }
-};
+    };
+}
 
 // global window object will listen for the network coming back online
 window.addEventListener('online', catchUpDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -11,20 +11,20 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
     // links within index.html doc
     '/https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    '/https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+    '/https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
 // mini project:
 const PRECACHE = 'precache-v1';
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = 'static-cache-v1';
 const RUNTIME = 'runtime';
-const DATA_CACHE_NAME = "data-cache-v1";
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
             .open(PRECACHE)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then((cache) => cache.getAll(FILES_TO_CACHE))
             .then(self.skipWaiting())
     );
 });
@@ -35,7 +35,9 @@ self.addEventListener('activate', (event) => {
         caches
             .keys()
             .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+                return cacheNames.filter(
+                    (cacheName) => !currentCaches.includes(cacheName)
+                );
             })
             .then((cachesToDelete) => {
                 return Promise.all(
